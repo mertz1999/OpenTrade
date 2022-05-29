d0b5e85b911558451b02889035f902c0c8fd07dd
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-05-29 20:09:10 +0430

    Complete Infinity Grid strategy

--- InfinityGrids.py
+++ InfinityGrids.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # Preparing TEST data
-data = pd.read_csv('./data/BTC_2018.csv')
+data = pd.read_csv('./data/BTC_2021.csv')
 data = pd.DataFrame(data.values[::-1], data.index, data.columns)
 
 data['date'] = pd.to_datetime(data['date'])
@@ -12,7 +12,7 @@ data['symbol']  = 'BTC'
 fee = 0.1
 investment   = 500
 lowest_price = 1000
-PPG          = 1  # In % (Profit-Per-Grid)
+PPG          = 0.5  # In % (Profit-Per-Grid)
 
 
 st = InfinityGrids(lowest_price, PPG, data, investment, fee, name="Infinity_grids", method="VOLUME")
--- OpenTrade/strategy_list/infinity_grids.py
+++ OpenTrade/strategy_list/infinity_grids.py
@@ -1,4 +1,6 @@
 from ..strategy import *
+import matplotlib.pyplot as plt 
+
 
 class InfinityGrids(Strategy):
     def __init__(self, lowest_price, PPG, data, invesment, fee, name='Infinity_grids', method="VOLUME"):
@@ -7,8 +9,12 @@ class InfinityGrids(Strategy):
         self.lowest_price = lowest_price
         self.PPG          = PPG
         self.saved        = 0.1 * invesment
+        self.saved_list   = []
+        self.price_list   = []
+        self.inv_list     = []
         self.invest       = 0.9 * invesment
         self.base_price   = 0.0
+        self.last_price   = 0.0
         
         # Print some information to user
         print(" ----------  LOADING -----------")
@@ -18,8 +24,14 @@ class InfinityGrids(Strategy):
 
 
     def algo(self, *args, **kwargs):
-        idx         = kwargs['idx']
-        close_price = kwargs['close_price']
+        idx             = kwargs['idx']
+        close_price     = kwargs['close_price']
+        self.last_price = close_price
+
+        self.saved_list.append(self.saved)
+        self.inv_list.append(self.trades.total_online_profit(close_price))
+        if idx < 30000:
+            self.price_list.append(close_price)
 
         # Buy when bot is running
         if self.trades.open_volume == 0:
@@ -40,8 +52,9 @@ class InfinityGrids(Strategy):
             # Update base price
             self.base_price = close_price
 
+
         # If price changing rate become lower than PPG value
-        elif (changing_rate < -1 * self.PPG) and not (self.saved < (self.invest * self.PPG/100)):
+        elif (changing_rate < -1 * self.PPG) and not (self.saved < (self.invest * self.PPG/100)) and (close_price > self.lowest_price):
             # find different
             diff = self.invest - self.trades.total_online_profit(close_price)
             # Buy surplus
@@ -50,6 +63,7 @@ class InfinityGrids(Strategy):
             self.saved -= diff 
             # Update base price 
             self.base_price = close_price
+
 
         else:
             pass
@@ -62,20 +76,23 @@ class InfinityGrids(Strategy):
 
 
     def result_info(self):
-        pass
+        plt.plot(self.saved_list)
+        plt.title("Saved values")
+        plt.show()
+
         print("\n-------------------- Results --------------------")
-        print("Amount of opening volume : {:.2f}".format(self.trades.invesment))
+        print("Amount of opening volume : {:.2f}".format(self.trades.total_online_profit(self.last_price)))
         print("Amount of Saved          : {:.2f}".format(self.saved))
         # print("APR                           : {:.2f}%".format((self.trades.total_profit()/self.invest_copy)*100))
         # print("Balance                       : {:.2f}".format(self.trades.invesment))
         # print("Total fees                    : {:.2f}".format(self.trades.fees))
         # print(len(self.trades.closed_positions))
 
-        # print("\nThanks to using ((Grid trading))")
+        print("\nThanks to using ((Infinity Grids))")
 
-        # self.trades.log("\n-------------------- Results --------------------")
-        # self.trades.log("Total profit in this strategy : {:.2f}".format(self.trades.total_profit()))
-        # self.trades.log("APR                           : {:.2f}%".format((self.trades.total_profit()/self.invest_copy)*100))
+        self.trades.log("\n-------------------- Results --------------------")
+        self.trades.log("Amount of opening volume : {:.2f}".format(self.trades.total_online_profit(self.last_price)))
+        self.trades.log("Amount of Saved          : {:.2f}".format(self.saved))
 
 
 
