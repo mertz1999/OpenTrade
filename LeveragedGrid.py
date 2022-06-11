16269aa51bb451a5fe71a43fd0a885e1dd4c8197
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-06-11 12:05:34 +0430

    Implement leverged grid and leveraged reverse grid based on parent grids bot

---
+++ LeveragedGrid.py
@@ -0,0 +1,23 @@
+from OpenTrade.strategy_list.grid_trading import GridTrading
+# import OpenTrade
+import pandas as pd
+
+data = pd.read_csv('./data/BTC_2021.csv')
+data = pd.DataFrame(data.values[::-1], data.index, data.columns)
+
+
+# Grid Trading parameters
+High_price         = 100000
+Low_price          = 10000
+num_grids          = 500 
+per_order          = 0.1
+investment         = 600
+fee                = 0.1        # In percent
+max_open_positions = 8
+leverage           = 3          # This will be mul. by investment
+
+investment = investment * leverage
+
+
+st = GridTrading(High_price , Low_price, num_grids, per_order, max_open_positions,data,investment, fee, name="Grid-Trading", method="ORDER")
+st.run()
